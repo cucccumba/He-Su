a17291b0b8aea4c81a5974b37854b2791710e36e
Ilia Artemev <55094588+cucccumba@users.noreply.github.com>
2021-12-05 14:53:13 +0300

    Update He_Su_impl.py

--- He_Su_impl.py
+++ He_Su_impl.py
@@ -1,4 +1,3 @@
-import numpy as np
 import random
 
 def rmspp(number, attempts=28):
@@ -79,7 +78,7 @@ def rabinMiller(num):
             i = 0
             while v != (num - 1):
                 if i == t - 1:
-                return False
+                    return False
             else:
                 i = i + 1
                 v = (v ** 2) % num
@@ -106,73 +105,190 @@ def isPrime(num):
     return rabinMiller(num)
 def generateLargePrime(keysize = 1024):
     while True:
-    num = random.randrange(2**(keysize-1), 2**(keysize))
-    if isPrime(num):
-        return num
+        num = random.randrange(2**(keysize-1), 2**(keysize))
+        if isPrime(num):
+            return num
 
 def generateKeys(keySize):
     # Step 1: Create two prime numbers, p and q. Calculate n = p * q.
-    print('Generating p prime...')
     p = generateLargePrime(keySize)
-    print('Generating q prime...')
     q = generateLargePrime(keySize)
     n = p * q
 	
     # Step 2: Create a number e that is relatively prime to (p-1)*(q-1).
-    print('Generating e that is relatively prime to (p-1)*(q-1)...')
     while True:
         e = random.randrange(2 ** (keySize - 1), 2 ** (keySize))
         if gcd(e, (p - 1) * (q - 1)) == 1:
             break
    
     # Step 3: Calculate d, the mod inverse of e.
-    print('Calculating d that is mod inverse of e...')
     d = findModInverse(e, (p - 1) * (q - 1))
     publicKey = (n, e)
     privateKey = (n, d)
-    print('Public key:', publicKey)
-    print('Private key:', privateKey)
     return (publicKey, privateKey)
 
 def egcd(a, b):
-    """
-    egcd(a, b) -> d, x, y, such as d == gcd(a, b) == ax + by
-    egcd is an Extended Greatest Common Divisor
-    http://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
-    """
-    if b == 0:
-        return (a, 1, 0)
+    if a == 0:
+        return (b, 0, 1)
     else:
-        d, x, y = egcd(b, a % b)
-        return d, y, x - y * (a / b)
-
+        g, y, x = egcd(b % a, a)
+        return (g, x - (b // a) * y, y)
 def mmi(a, m):
-    """
-    mmi(a, m) -> x, such as ax % m = 1
-    mmi is a Modular Multiplicative Inverse
-    See http://en.wikipedia.org/wiki/Modular_multiplicative_inverse
-    """
-    gcd, x, q = egcd(a, m)
-    if gcd != 1:
-        # The a and m are not coprimes, so the inverse doesn't exist
-        return None
+    g, x, y = egcd(a, m)
+    if g != 1:
+        raise Exception('modular inverse does not exist')
     else:
-        return (x + m) % m
+        return x % m
+
+def _hash(x):
+    return hash(x) % 200
 
 class Voter(object):
-    def __init__(self):
-        self.open_key = None
+    def __init__(self, name, public_key, private_key, mask_factor, public_adm_key):
+        self.name = name
+        self.public_key = public_key
+        self.private_key = private_key
+        self.mask_factor = mask_factor
+        self.public_adm_key = public_adm_key
+        self.signed_public_key = None
+        self.hash_key = None
         self.secret_key = None
-        self.mask_factor = None
         
+    def authorization(self):
+        self.hash_key = _hash(self.public_key)
+        f = mod_exp(self.mask_factor, self.public_adm_key[1], self.public_adm_key[0]) * self.hash_key
+        return f
+        
+    def getSignedKey(self, sign):
+        inverse =  mmi(self.mask_factor, self.public_adm_key[0])
+        self.signed_public_key = sign * inverse
+        encrypted = mod_exp(self.signed_public_key, self.public_adm_key[1], self.public_adm_key[0])
+        if (encrypted == self.hash_key):
+            print(self.name + ": Signed key is OK")
+        return (self.public_key, self.signed_public_key)
+        
+    def make_vote(self, vote, secret_key):
+        self.secret_key = secret_key
+        encrypt_vote = mod_exp(vote, self.secret_key[1], self.secret_key[0])
+        sign_vote = mod_exp(_hash(encrypt_vote), self.private_key[1], self.private_key[0])
+        return (self.public_key, encrypt_vote, sign_vote)
+    
+    def confirm_vote(self):
+        sign = mod_exp(_hash(self.secret_key), self.private_key[1], self.private_key[0])
+        return (self.public_key, self.secret_key, sign)
 class Admin(object):
-    def __init__(self):
-        self.open_key = None
-        self.secret_key = None
-        self.mask_factor = None
+    def __init__(self, public_key, private_key):
+        self.public_key = public_key
+        self.private_key = private_key
+        self.auth_voters = []
+    
+    def sign(self, auth, name):
+        self.auth_voters.append(name)
+        return mod_exp(auth, self.private_key[1], self.private_key[0])
         
+class Counter(object):
+    def __init__(self, public_adm_key):
+        self.public_adm_key = public_adm_key
+        self.auth_keys = []
+        self.secret_key = None
+        self.votes = []
+        
+    def registrate(self, pair):
+        signed_public_key = pair[1]
+        encrypted = mod_exp(signed_public_key, self.public_adm_key[1], self.public_adm_key[0])
+        if (encrypted == _hash(pair[0])):
+            self.auth_keys.append(pair[0])
+            print("Key registrated: ", pair[0])
+            return 1
+        else:
+            return 0
+    
+    def generateSecretKey(self):
+        secret_key = generateKeys(5)
+        self.secret_key = secret_key
+        return secret_key[0]
+    
+    def public_vote(self, vote):
+        if (self.auth_keys.__contains__(vote[0])):
+            print("Key auth: ", vote[0])
+            encrypted = mod_exp(vote[2], vote[0][1], vote[0][0])
+            if (encrypted == _hash(vote[1])):
+                self.votes.append(vote)
+                print("Initial vote ", vote)
+                return 1
+        return 0
+    
+    def public_final_vote(self, confirm):
+        encrypt = mod_exp(confirm[2], confirm[0][1], confirm[0][0])
+        if (encrypt == _hash(confirm[1])):
+            vote = None
+            print("Confirm is OK")
+            for i in self.votes:
+                if (i[0] == confirm[0]):
+                    vote = i
+            if (vote != None):
+                encrypted_vote = mod_exp(vote[1], self.secret_key[1][1], self.secret_key[1][0])
+                print("Vote: ", (encrypted_vote, vote[1], self.secret_key[0], vote[2], confirm[2], vote[0]))
+    
 def getMaskFactor():
     return random.randint(1, 50)
+
+def vote():
+    admin_keys = generateKeys(5)
+    print("admin keys: ", admin_keys)
+    admin = Admin(admin_keys[0], admin_keys[1])
+    
+    counter = Counter(admin_keys[0])
+   
+    voter1_keys = generateKeys(5)
+    voter2_keys = generateKeys(5)
+    print("voter1 keys:", voter1_keys)
+    print("voter2 keys:", voter2_keys)
+    mask_factor1 = getMaskFactor()
+    print("mask_factor1: ", mask_factor1)
+    mask_factor2 = getMaskFactor()
+    print("mask_factor2: ", mask_factor2)
+    voter1 = Voter("Voter1", voter1_keys[0], voter1_keys[1], mask_factor1, admin_keys[0])
+    voter2 = Voter("Voter2", voter2_keys[0], voter2_keys[1], mask_factor2, admin_keys[0])
+    
+    f1 = voter1.authorization()
+    print("f1: ", f1)
+    f2 = voter2.authorization()
+    print("f2: ", f2)
+   
+    sign1 = admin.sign(f1, voter1.name)
+    print("sign1: ", sign1)
+    sign2 = admin.sign(f2, voter2.name)
+   
+    print("sign2: ", sign2)
+    print("Auth voters: ", admin.auth_voters)
+    
+    pair1 = voter1.getSignedKey(sign1)
+    print("pair1: ", pair1)
+    pair2 = voter2.getSignedKey(sign2)
+    print("pair2: ", pair2)
+    
+    res1 = counter.registrate(pair1)
+    print("res1: ", res1)
+    res2 = counter.registrate(pair2)
+    print("res2: ", res2)
+    
+    print("Auth keys: ", counter.auth_keys)
+    
+    secret_key = counter.generateSecretKey()
+    
+    vote1 = voter1.make_vote(2, secret_key)
+    print("vote1: ", vote1)
+    vote2 = voter2.make_vote(3, secret_key)
+    print("vote2: ", vote2)
+    
+    if(counter.public_vote(vote1) == 1):
+        confirm1 = voter1.confirm_vote()
+        print("confirm1: ", confirm1)
         
-def authorization(admin_key, key, mask):
-    return mod_exp(mask, key[0][1], key[0][0]) * hash(admin_key[0])
+        counter.public_final_vote(confirm1)
+    if(counter.public_vote(vote2) == 1):
+        confirm2 = voter2.confirm_vote()
+        print("confirm2: ", confirm2)
+        
+        counter.public_final_vote(confirm2)
